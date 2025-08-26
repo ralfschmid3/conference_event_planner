e232271886331f26ce4053b7aa66143b17a4fd6d
ralfschmid3 <ralf.schmid3@gmx.de>
2025-08-26 16:19:27 +0200

    "Finished Task 4 -> add-ons section"

--- src/avSlice.js
+++ src/avSlice.js
@@ -1,20 +1,56 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const avSlice = createSlice({
-  name: "av",
-  initialState: [
-    
-  ],
+    name: "av",
+    initialState: [
+        {
+            img: "/projector_woman-7136446_1280.jpg",
+            name: "Projectors",
+            cost: 200,
+            quantity: 0,
+	},
+	{
+            img: "/speaker-971975_1280.png",
+            name: "Speaker",
+            cost: 35,
+            quantity: 0,
+	},
+	{
+            img: "/microphone-5340340_1280.jpg",
+            name: "Microphones",
+            cost: 45,
+            quantity: 0,
+	},
+	{
+            img: "/white-board-593309_1280.jpg",
+            name: "Whiteboards",
+            cost: 80,
+            quantity: 0,
+	},
+
+	{
+            img: "/signage_work-harder-1845901_1280.jpg",
+            name: "Signage",
+            cost: 80,
+            quantity: 0,
+	},
+    ],
 
 
-  reducers: {
-    incrementAvQuantity: (state, action) => {
-      
+    reducers: {
+	incrementAvQuantity: (state, action) => {
+	    const item = state[action.payload];
+	    if (item) {
+		item.quantity++;
+	    }
+	},
+	decrementAvQuantity: (state, action) => {
+	    const item = state[action.payload];
+	    if (item && item.quantity > 0) {
+		item.quantity--;
+	    }
+	},
     },
-    decrementAvQuantity: (state, action) => {
-     
-    },
-  },
 });
 
 export const { incrementAvQuantity, decrementAvQuantity } = avSlice.actions;
